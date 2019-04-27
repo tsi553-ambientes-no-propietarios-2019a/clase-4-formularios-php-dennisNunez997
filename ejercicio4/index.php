9a1e1769465b1dbb9c8997ca5bb65ab4a1b1fe47
dennis <dennis.nunez@epn.edu.ec>
2019-04-27 17:42:01 -0500

    ejercicio 4

--- ejercicio4/index.php
+++ ejercicio4/index.php
@@ -11,3 +11,46 @@
  * "Las contrseñas no coinciden".
  */
 ?>
+
+
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <title>registro</title>
+        <meta charset= "UTF-8" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    </head>
+
+    <body>
+        <div class = "container">
+            <form action = "usuario.php" method = "POST">
+                <h1 class = 'page-header'>Registro de usuario</h1>
+
+                <div class= "form-group">
+                    <label>Ingrese su nombre</label>
+                    <input type = "text" name = "nombre" class = "form-control" style="width:300px;height:30px"/>
+                </div>
+
+                <div class= "form-group">
+                    <label>Ingrese su contraseña</label>
+                    <input type = "password" name = "contraseña" class = "form-control" style="width:300px;height:30px"/>
+                </div>
+
+                <div class= "form-group">
+                    <label>Ingrese de nuevo su contraseña</label>
+                    <input type = "password" name = "verificar_contraseña" class = "form-control" style="width:300px;height:30px"/>
+                </div>
+
+                <div class = "form-group">
+                    
+                    <button class="btn btn-primary" type="submit">Iniciar</button>
+
+                </div>
+            </form>
+        </div>
+    </body>
+
+</html>
