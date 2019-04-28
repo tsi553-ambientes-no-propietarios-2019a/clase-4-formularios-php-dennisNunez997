8bfb984cff969cf0d6f39b471d2c07fee1a5658c
dennis <dennis.nunez@epn.edu.ec>
2019-04-27 22:19:34 -0500

    ejercicio7

--- ejercicio7/index.php
+++ ejercicio7/index.php
@@ -4,3 +4,37 @@ Escribir código PHP que inicialice un arreglo con los números del 1 al 365. De
 */
 
 ?>
+
+<!DOCTYPE html>
+ 
+<html lang="es">
+ 
+<head>
+<title>Dias del año</title>
+<meta charset="utf-8" />
+<link rel="stylesheet" href="estilos.css" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+ 
+<body>
+
+    <div class = "container">
+        <h1 class = 'page-header'>primer formulario</h1>
+        <form action = "numeros.php" method = "POST"> 
+            <div class = "form-group">
+                <label>Ingrese un numero</label>
+                <input type = "text" name = "numero" class = "form-control" style="width:200px;height:30px"/>
+            </div>
+            <div class = "form-group">
+               
+               <button class="btn btn-primary" type="submit">Enviar</button>
+
+            </div>
+        </form>
+    </div>
+
+
+</body>
+</html>
