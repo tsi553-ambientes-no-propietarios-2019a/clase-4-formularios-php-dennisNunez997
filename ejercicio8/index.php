a58b0af48aace1d9c5c73bda4802618e68823788
dennis <dennis.nunez@epn.edu.ec>
2019-04-29 22:42:01 -0500

    ejercicio8

--- ejercicio8/index.php
+++ ejercicio8/index.php
@@ -4,3 +4,28 @@
 - Utilizar HTML y CSS para dar estilo a la lista. Cada mes debe mostrarse de diferente color junto con la lista de días. Los meses deben aparecer con negrita. Pista: se puede utilizar elementos ul y li para mostrar la lista.
 
 */
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <link rel = "stylesheet" href="estilo.css">
+        <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+    </head>
+    
+    <body>
+        <h1>
+        <form action = "dias.php" method = "POST">
+            <div class = "form-group">
+                <label>Ingrese el numero: </label>
+                <input type = "text" name = "numero" class = "form-control" style="width:200px;height:30px">
+            </div>
+            <div class = "form-group">
+                <button class="btn btn-primary" type="submit">Enviar</button>
+            </div>
+        </form>
+    </body>
+
+</html>
